33d368ab2d4928e0382149f6daf9c91bef6f17d6
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2019-07-23 14:33:00 -0500

    refactor: upgrade to web3 v1.2.0
    Upgrade all dependencies on web3/web3-* v1.0.0-beta.37 to v1.2.0.
    
    Make various adjustments related to the previous convention of
    `"web3": "1.0.0-beta"` in `embark.json` signifying that embark's own web3
    dependency should be used in dapp builds.
    
    Fix bugs in library manager, including a switch from using the
    live-plugin-manager package to using npm in a child process to install
    `"versions"` dependencies specified in `embark.json` when a specified version
    doesn't match up with embark's own version for that package.
    
    Avoid race conditions when installing `"versions"` by completing all installs
    prior to starting other services. If an install fails, then after all the
    installs have completed or failed the embark command will exit with error.
    
    Change various comments and update docs to reflect the new default of web3
    v1.2.0.

--- packages/embark/src/cmd/cmd_controller.js
+++ packages/embark/src/cmd/cmd_controller.js
@@ -123,6 +123,9 @@ class EmbarkController {
         });
       },
       function (callback) {
+        engine.startService("libraryManager").installAll((err) => callback(err ? err : null));
+      },
+      function (callback) {
         let pluginList = engine.plugins.listPlugins();
         if (pluginList.length > 0) {
           engine.logger.info(__("loaded plugins") + ": " + pluginList.join(", "));
@@ -133,7 +136,6 @@ class EmbarkController {
         engine.startService("coreProcess");
         engine.startService("blockchainListener");
         engine.startService("serviceMonitor");
-        engine.startService("libraryManager");
         engine.startService("codeRunner");
         engine.startService("pipeline");
         engine.startService("deployment");
@@ -189,6 +191,7 @@ class EmbarkController {
       if (err) {
         engine.logger.error(err.message);
         engine.logger.info(err.stack);
+        process.exit(1);
       } else {
         engine.events.emit('firstDeploymentDone');
       }
@@ -221,6 +224,9 @@ class EmbarkController {
       function initEngine(callback) {
         engine.init({}, callback);
       },
+      function (callback) {
+        engine.startService("libraryManager").installAll((err) => callback(err ? err : null));
+      },
       function startServices(callback) {
         let pluginList = engine.plugins.listPlugins();
         if (pluginList.length > 0) {
@@ -229,7 +235,6 @@ class EmbarkController {
 
         if (!options.onlyCompile) engine.startService("web3");
         engine.startService("processManager");
-        engine.startService("libraryManager");
         engine.startService("codeRunner");
         engine.startService("pipeline");
         engine.startService("codeGenerator");
@@ -300,6 +305,9 @@ class EmbarkController {
       function initEngine(callback) {
         engine.init({}, callback);
       },
+      function (callback) {
+        engine.startService("libraryManager").installAll((err) => callback(err ? err : null));
+      },
       function startServices(callback) {
         let pluginList = engine.plugins.listPlugins();
         if (pluginList.length > 0) {
@@ -319,7 +327,6 @@ class EmbarkController {
         engine.startService("processManager");
         engine.startService("coreProcess");
         engine.startService("serviceMonitor");
-        engine.startService("libraryManager");
         engine.startService("pipeline");
         engine.startService("storage");
         engine.startService("cockpit");
@@ -364,6 +371,7 @@ class EmbarkController {
       if (err) {
         engine.logger.error(err.message);
         engine.logger.info(err.stack);
+        process.exit(1);
       } else {
         engine.events.emit('firstDeploymentDone');
       }
@@ -389,6 +397,9 @@ class EmbarkController {
         engine.init({}, callback);
       },
       function (callback) {
+        engine.startService("libraryManager").installAll((err) => callback(err ? err : null));
+      },
+      function (callback) {
         let pluginList = engine.plugins.listPlugins();
         if (pluginList.length > 0) {
           engine.logger.info(__("loaded plugins") + ": " + pluginList.join(", "));
@@ -396,7 +407,6 @@ class EmbarkController {
 
         engine.startService("processManager");
         engine.startService("serviceMonitor");
-        engine.startService("libraryManager");
         engine.startService("compiler");
         engine.startService("codeGenerator");
         engine.startService("graph");
@@ -408,11 +418,11 @@ class EmbarkController {
         engine.logger.error(err.message);
         engine.logger.info(err.stack);
       } else {
-
         engine.events.request("graph:create", options, () => {
           engine.logger.info(__("Done. %s generated", options.output).underline);
         });
       }
+
       process.exit(err ? 1 : 0);
     });
 
@@ -485,6 +495,9 @@ class EmbarkController {
       function initEngine(callback) {
         engine.init({}, callback);
       },
+      function (callback) {
+        engine.startService("libraryManager").installAll((err) => callback(err ? err : null));
+      },
       function startServices(callback) {
         engine.startService("scaffolding");
         callback();
@@ -502,7 +515,6 @@ class EmbarkController {
         }
         engine.startService("web3");
         engine.startService("processManager");
-        engine.startService("libraryManager");
         engine.startService("codeRunner");
         engine.startService("deployment", {onlyCompile: true});
 
@@ -522,11 +534,11 @@ class EmbarkController {
       if (err) {
         engine.logger.error(__("Error generating the UI: "));
         engine.logger.error(err.message || err);
-        process.exit(1);
       }
       engine.logger.info(__("finished generating the UI").underline);
       engine.logger.info(__("To see the result, execute {{cmd}} and go to /{{contract}}.html", {cmd: 'embark run'.underline, contract: options.contract}));
-      process.exit(0);
+
+      process.exit(err ? 1 : 0);
     });
   }
 
@@ -565,12 +577,14 @@ class EmbarkController {
           callback();
         });
       },
+      function (callback) {
+        engine.startService("libraryManager").installAll((err) => callback(err ? err : null));
+      },
       function startServices(callback) {
 
         engine.startService("web3");
         engine.startService("processManager");
         engine.startService("serviceMonitor");
-        engine.startService("libraryManager");
         engine.startService("codeRunner");
         engine.startService("pipeline");
         engine.startService("deployment");
@@ -653,10 +667,12 @@ class EmbarkController {
       function initEngine(callback) {
         engine.init({}, callback);
       },
+      function (callback) {
+        engine.startService("libraryManager").installAll((err) => callback(err ? err : null));
+      },
       function startServices(callback) {
         engine.startService("web3", {wait: true, node: options.node});
         engine.startService("processManager");
-        engine.startService("libraryManager");
         engine.startService("codeRunner");
         engine.startService("deployment", {
           trackContracts: false,
